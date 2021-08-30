fe52fe1c3b9ec5ef9d7a52996b318e338e242035
chihab1991 <chihab.chergui.1991@gmail.com>
2021-08-30 21:02:41 +0100

    DesktopVersionCompleted

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@ const loaderWrapper = document.querySelector(".loader-wrapper");
 window.addEventListener("load", () => {
   loaderWrapper.style.display = "none";
 });
+
+// fixed navbar
+window.addEventListener("scroll", function () {
+  if (window.scrollY > window.innerHeight) {
+    document.querySelector(".navbar").classList.add("fixed");
+  } else {
+    document.querySelector(".navbar").classList.remove("fixed");
+  }
+});
