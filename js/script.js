1e3b4473adf5c2a76aeeeea53d575b64298a3c24
chihab1991 <chihab.chergui.1991@gmail.com>
2021-09-12 23:39:52 +0100

    FullVersionOfWebsiteCompleted

--- js/script.js
+++ js/script.js
@@ -8,7 +8,16 @@ window.addEventListener("load", () => {
 window.addEventListener("scroll", function () {
   if (window.scrollY > window.innerHeight) {
     document.querySelector(".navbar").classList.add("fixed");
-  } else {
-    document.querySelector(".navbar").classList.remove("fixed");
   }
+});
+// screen width
+const scrwidth = document.querySelector(".width");
+window.addEventListener("resize", () => {
+  scrwidth.innerHTML = window.innerWidth;
+});
+
+// animation on Show
+const aboutMe = document.querySelector("#about-me");
+aboutMe.addEventListener("show", () => {
+  aboutMe.classList.add("about-me-anim");
 });
